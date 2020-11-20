b985e35150b7bee962a4acfa0abf06824ae8714d
Daniel Gonzalez <cadagoro@gmail.com>
2020-11-20 17:13:41 -0300

    Se agrego el archivo main.js de Javascript el cual permite el efecto que  el menu del (navbar)  baje al realizar scroll y al realizar scroll tambien se muestre un efecto de conteo a los numeros de los usuarios  /  Tambien se inserto los estilos del menu en SCSS/CSS: Archivo Js.scss

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// STICKY NAVEGATION
+
+let navbar = $(".navbar");
+
+
+$(window).scroll(function () {
+    // get the complete hight of window
+    let oTop = $(".section_2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+});
+
+
+
+// COUNTER ANIMATION
+
+let nCount = selector => {
+    $(selector).each(function () {
+        $(this).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: "swing",
+            step: function (value) {
+                $(this).text(Math.ceil(value));
+
+            }
+        });
+    });
+};
+
+let a = 0;
+
+$(window).scroll(function () {
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect > h2")
+    }
+});
